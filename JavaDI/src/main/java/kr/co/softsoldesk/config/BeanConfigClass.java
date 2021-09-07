523854e7796b217e47c87c44b5c6c2f382dadd4b
beerh <beerh@COM-Yeonnie>
2021-09-07 16:59:01 +0900

    120907 javadi

--- JavaDI/src/main/java/kr/co/softsoldesk/config/BeanConfigClass.java
+++ JavaDI/src/main/java/kr/co/softsoldesk/config/BeanConfigClass.java
@@ -1,10 +1,15 @@
 package kr.co.softsoldesk.config;
 
+import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import kr.co.softsoldesk.beans.DataBean1;
+import kr.co.softsoldesk.beans.DataBean2;
+import kr.co.softsoldesk.beans.DataBean3;
 import kr.co.softsoldesk.beans.TestBean1;
+import kr.co.softsoldesk.beans.TestBean2;
+import kr.co.softsoldesk.beans.TestBean3;
 
 @Configuration
 public class BeanConfigClass {
@@ -23,5 +28,34 @@ public class BeanConfigClass {
 		t1.setData3(new DataBean1());
 		
 		return t1;
+	}
+	
+	//autowire="byName"
+	@Bean
+	public DataBean2 data1() {
+		
+		return new DataBean2();
+	}
+	
+	@Bean
+	public DataBean2 data2() {
+		
+		return new DataBean2();
+	}
+	
+	//spring framework 5.1부터는 권장하지 않고 생성자를 통해서 주입을 권장함
+	@Bean(autowire=Autowire.BY_NAME)
+	public TestBean2 java3() {
+		return new TestBean2();
+	}
+	
+	@Bean
+	public DataBean3 data100() {
+		return new DataBean3();
+	}
+	
+	@Bean(autowire=Autowire.BY_TYPE)
+	public TestBean3 java4() {
+		return new TestBean3();
 	}
 }
--- JavaDI/src/main/java/kr/co/softsoldesk/main/MainClass.java
+++ JavaDI/src/main/java/kr/co/softsoldesk/main/MainClass.java
@@ -1,10 +1,12 @@
 package kr.co.softsoldesk.main;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import kr.co.softsoldesk.beans.TestBean1;
 import kr.co.softsoldesk.beans.TestBean2;
 import kr.co.softsoldesk.beans.TestBean3;
+import kr.co.softsoldesk.config.BeanConfigClass;
 
 public class MainClass {
 
@@ -33,6 +35,36 @@ public class MainClass {
 		TestBean3 xml4= ctx.getBean("xml4", TestBean3.class);
 		System.out.printf("xml4 : %s\n", xml4.getData1());
 		System.out.printf("xml4 : %s\n", xml4.getData2());
+		
+		ctx.close();
+		
+		System.err.println("==========================Annotation============================");
+		
+		AnnotationConfigApplicationContext ctx2 = new AnnotationConfigApplicationContext(BeanConfigClass.class);
+		
+		System.err.println("----------생성자를 통한 주입----------");
+		TestBean1 java1= ctx2.getBean("java1", TestBean1.class);
+		System.out.printf("java1 : %d\n", java1.getData1());
+		System.out.printf("java1 : %s\n", java1.getData2());
+		System.out.printf("java1 : %s\n", java1.getData3());
+		
+		System.err.println("----------setter를 통한 주입----------");
+		TestBean1 java2= ctx2.getBean("java2", TestBean1.class);
+		System.out.printf("java2 : %d\n", java2.getData1());
+		System.out.printf("java2 : %s\n", java2.getData2());
+		System.out.printf("java2 : %s\n", java2.getData3());
+		
+		System.err.println("----------autowire=byName을 통한 주입----------");
+		TestBean2 java3= ctx2.getBean("java3", TestBean2.class);
+		System.out.printf("java3 : %s\n",java3.getData1());
+		System.out.printf("java3 : %s\n", java3.getData2());
+		
+		System.err.println("----------autowire=byType을 통한 주입----------");
+		TestBean3 java4= ctx2.getBean("java4", TestBean3.class);
+		System.out.printf("java4 : %s\n", java4.getData1());
+		System.out.printf("java4 : %s\n", java4.getData2());
+		
+		ctx2.close();
 		
 	}
 
